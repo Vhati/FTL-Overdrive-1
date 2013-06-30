c456b650b28151f6809b680ebcd73f9a1b154046
Vhati <tvtronix@yahoo.com>
2013-06-30 15:39:35 -0400

    Removed hardcoded java path

--- _overdrive_run.bat
+++ _overdrive_run.bat
@@ -3,8 +3,6 @@ SETLOCAL
 
 CD /D "%~dp0"
 
-SET JAVA_CMD=D:\Apps\j2sdk1.6.0_13\bin\java.exe
-
 SET APP_JAR=overdrive.jar
 
 SET CLASSPATH=.
@@ -13,7 +11,7 @@ SET CLASSPATH=%CLASSPATH%;lib\jars\lwjgl.jar
 SET CLASSPATH=%CLASSPATH%;lib\jars\xpp3-1.1.4c.jar
 SET CLASSPATH=%CLASSPATH%;lib\jars\TWL.jar
 
-"%JAVA_CMD%" -Djava.library.path="lib\natives" -cp "%CLASSPATH%" com.ftloverdrive.OverDrive
+java.exe -Djava.library.path="lib\natives" -cp "%CLASSPATH%" com.ftloverdrive.OverDrive
 
 PAUSE
 ENDLOCAL
